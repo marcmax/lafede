bdf4d4eb4850814a7c6bfdc7b4eb0082e4ed0ea6
marcmax557 <marquitoevaristo@gmail.com>
2026-01-13 20:16:43 -0300

    se agregaron imagenes al body con texto al lado en forma de zig zag, se comprimieron las imagenes para que cargue mas rapido y se coloco el loading de las img en lazy

--- script.js
+++ script.js
@@ -8,3 +8,20 @@ document.getElementById('close-menu').addEventListener('click', function() {
     const sidebar = document.getElementById('sidebar');
     sidebar.classList.remove('open'); // Cerrar el menú
 });
+
+  const logoImg = document.querySelector('.logo-image');
+        logoImg.addEventListener('mouseenter', () => {
+            logoImg.style.opacity = '0';
+            setTimeout(() => {
+                logoImg.src = 'images/logo_color.png';
+                logoImg.style.opacity = '1';
+            }, 100);
+        });
+        logoImg.addEventListener('mouseleave', () => {
+            logoImg.style.opacity = '0';
+            setTimeout(() => {
+                logoImg.src = 'images/logo_blanco.png';
+                logoImg.style.opacity = '1';
+            }, 100);
+        });
+    
